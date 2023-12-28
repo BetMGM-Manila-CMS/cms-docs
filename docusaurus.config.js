d37a13bacef612ccba6202c70ac63eb47b2f6919
Kyle-Atienza <atienzakylefrancis@gmail.com>
2023-12-28 16:52:26 +0800

    fix footer broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,8 +118,8 @@ const config = {
             title: "Docs",
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/intro",
+                label: "Builds",
+                to: "/docs/builds/assets-introduction",
               },
             ],
           },
