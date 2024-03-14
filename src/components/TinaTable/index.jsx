f5d728ccd2212673fae45ed854e7fe2289d734c7
Kyle-Atienza <kylefrancisedison@gmail.com>
2024-03-14 17:16:28 +0800

    pat eds

--- src/components/TinaTable/index.jsx
+++ src/components/TinaTable/index.jsx
@@ -23,101 +23,105 @@ export const TinaTableRow = ({ children }) => {
 }
 
 export const TinaTable = ({ children, topHeader, leftHeader, columnWidth, className }) => {
-    let items = children
+    if (children) {
 
-    if (leftHeader && children.length) {
-        items = children.map(row => ({
-            ...row,
-            props: {
-                ...row.props,
-                children: row.props.children.map((cell, index) => {
-                    if (index === 0) {
-                        return {
-                            ...cell,
-                            props: {
-                                ...cell.props,
-                                header: true
+        let items = children
+
+        if (leftHeader && children.length) {
+            items = children.map(row => ({
+                ...row,
+                props: {
+                    ...row.props,
+                    children: row.props.children.map((cell, index) => {
+                        if (index === 0) {
+                            return {
+                                ...cell,
+                                props: {
+                                    ...cell.props,
+                                    header: true
+                                }
                             }
+                        } else {
+                            return cell
                         }
-                    } else {
-                        return cell
-                    }
-                })
+                    })
+                }
+            }))
+        }
+
+        if (columnWidth) {
+            const widths = columnWidth.split(', ').map(val => Number(val))
+            const firstRow = items[0]
+
+            const topRow = {
+                ...firstRow,
+                props: {
+                    ...firstRow.props,
+                    children: firstRow.props.children.map((cell, index) => ({
+                        ...cell,
+                        props: {
+                            ...cell.props,
+                            className: `w-[${widths[index]}%]`
+                        }
+                    }))
+                }
             }
-        }))
-    }
 
-    if (columnWidth) {
-        const widths = columnWidth.split(', ').map(val => Number(val))
-        const firstRow = items[0]
+            items = [topRow, ...items.slice(1)]
+        }
 
-        const topRow = {
-            ...firstRow,
-            props: {
-                ...firstRow.props,
-                children: firstRow.props.children.map((cell, index) => ({
+        // head
+        let head
+        if (topHeader) {
+            if (items.length) {
+                head = items[0].props.children.map(cell => ({
                     ...cell,
                     props: {
                         ...cell.props,
-                        className: `w-[${widths[index]}%]`
+                        header: true,
                     }
                 }))
+            } else {
+                head = [items.props.children.map(cell => ({
+                    ...cell,
+                    props: {
+                        ...cell.props,
+                        header: true,
+                    }
+                }))]
             }
         }
 
-        items = [topRow, ...items.slice(1)]
-    }
-
-    // head
-    let head
-    if (topHeader) {
-        if (items.length) {
-            head = items[0].props.children.map(cell => ({
-                ...cell,
-                props: {
-                    ...cell.props,
-                    header: true,
-                }
-            }))
+        // bidy
+        let body
+        if (head) {
+            if (items.length > 1) {
+                body = items.slice(1)
+            }
         } else {
-            head = [items.props.children.map(cell => ({
-                ...cell,
-                props: {
-                    ...cell.props,
-                    header: true,
+            if (items.length > 1) {
+                body = items
+            } else {
+                body = [items]
+            }
+        }
+
+        return (
+            <table className={className}>
+                {
+                    head ? <thead>
+                        <tr>
+                            {head}
+                        </tr>
+                    </thead> : null
                 }
-            }))]
-        }
+                {
+                    body ? <tbody>
+                        {body}
+                    </tbody> : null
+                }
+            </table>
+        )
     }
 
-    // bidy
-    let body
-    if (head) {
-        if (items.length > 1) {
-            body = items.slice(1)
-        }
-    } else {
-        if (items.length > 1) {
-            body = items
-        } else {
-            body = [items]
-        }
-    }
-
-    return (
-        <table className={className}>
-            {
-                head ? <thead>
-                    <tr>
-                        {head}
-                    </tr>
-                </thead> : null
-            }
-            {
-                body ? <tbody>
-                    {body}
-                </tbody> : null
-            }
-        </table>
-    )
 }
