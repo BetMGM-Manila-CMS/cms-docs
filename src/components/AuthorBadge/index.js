839918cf33b2a175e722f81f22d1bd596a8ec98f
Kyle-Atienza <kylefrancisedison@gmail.com>
2024-05-03 08:57:04 +0800

    fix github api

--- src/components/AuthorBadge/index.js
+++ src/components/AuthorBadge/index.js
@@ -1,4 +1,6 @@
 export const AuthorBadge = ({ name, avatar, className }) => {
+  console.log(name)
+
   const initials = name
     .split(" ")
     .map((part) => part.slice(0, 1).toUpperCase())
@@ -9,13 +11,13 @@ export const AuthorBadge = ({ name, avatar, className }) => {
     <div className={`flex items-center gap-2 ${className} text-sm`}>
       {
         avatar ?
-        <img class="w-8 h-8 m-0 rounded-full" src={avatar} alt="Rounded avatar" />
-        :
-        <div className="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-          <span className="font-medium text-gray-600 dark:text-gray-300">
-            {initials}
-          </span>
-        </div>
+          <img class="w-8 h-8 m-0 rounded-full" src={avatar} alt="Rounded avatar" />
+          :
+          <div className="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <span className="font-medium text-gray-600 dark:text-gray-300">
+              {initials}
+            </span>
+          </div>
       }
       <span>{name}</span>
     </div>
--- src/theme/DocItem/index.js
+++ src/theme/DocItem/index.js
@@ -34,21 +34,22 @@ export default function DocItem(props) {
       .then((res) => {
         const commits = res.data
 
+        console.log(commits)
+
         setContributors(
-          commits.map(commit => ({
-            name: commit.author.login,
-            avatar: commit.author.avatar_url
+          commits.map((commit) => ({
+            name: commit.author ? commit.author.login : commit.commit.author.name,
+            avatar: commit.author ? commit.author.avatar_url : null
           }))
-          .reduce((commits, commit) => {
-            if (!commits.filter(commitItem => commitItem.name === commit.name).length) {
-              commits.push(commit)
-            }
-            return commits
-          }, [])
+            .reduce((commits, commit) => {
+              if (!commits.filter(commitItem => commitItem.name === commit.name).length) {
+                commits.push(commit)
+              }
+              return commits
+            }, [])
         )
-      
+
         setLastUpdate(commits[0])
-        console.log(lastUpdate)
       })
       .catch((e) => {
         console.log(e)
@@ -62,43 +63,43 @@ export default function DocItem(props) {
   const { last_update, authors } = props.content.frontMatter;
 
   return (
-      <DocProvider content={props.content}>
-        <HtmlClassNameProvider className={docHtmlClassName}>
-          <DocItemMetadata />
-          <DocItemLayout>
-            <MDXComponent />
-            <div className="mt-8 pt-8 border-t-[1px] border-primary md:flex">
-              {lastUpdate?.commit ? (
-                <div className="w-full md:w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-neutral-800 dark:border-gray-700 transition">
-                  <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Last Update
+    <DocProvider content={props.content}>
+      <HtmlClassNameProvider className={docHtmlClassName}>
+        <DocItemMetadata />
+        <DocItemLayout>
+          <MDXComponent />
+          <div className="mt-8 pt-8 border-t-[1px] border-primary md:flex">
+            {lastUpdate ?.commit ? (
+              <div className="w-full md:w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-neutral-800 dark:border-gray-700 transition">
+                <h5 className=" text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Last Update
                   </h5>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {lastUpdate?.commit.message}
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {lastUpdate ?.commit.message}
+                </p>
+                <div className="flex items-center justify-between mt-5">
+                  <AuthorBadge name={lastUpdate ?.author.login} avatar={lastUpdate ?.author.avatar_url} />
+                  <p className="text-xs text-end">
+                    {new Date(lastUpdate ?.commit.author.date).toDateString()}
                   </p>
-                  <div className="flex items-center justify-between mt-5">
-                    <AuthorBadge name={lastUpdate?.author.login} avatar={lastUpdate?.author.avatar_url} />
-                    <p className="text-xs text-end">
-                      {new Date(lastUpdate?.commit.author.date).toDateString()}
-                    </p>
-                  </div>
                 </div>
-              ) : null}
-              {contributors.length ? (
-                <div className="w-full mt-8 md:mt-0 md:text-right md:pl-5 md:w-1/2">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    Contributors
+              </div>
+            ) : null}
+            {contributors.length ? (
+              <div className="w-full mt-8 md:mt-0 md:text-right md:pl-5 md:w-1/2">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Contributors
                   </h5>
-                  <div className="flex flex-col gap-2 mt-5 md:items-end">
-                    {contributors.map(({name, avatar}, index) => {
-                      return <AuthorBadge name={name} avatar={avatar} key={index} />;
-                    })}
-                  </div>
+                <div className="flex flex-col gap-2 mt-5 md:items-end">
+                  {contributors.map(({ name, avatar }, index) => {
+                    return <AuthorBadge name={name} avatar={avatar} key={index} />;
+                  })}
                 </div>
-              ) : null}
-            </div>
-          </DocItemLayout>
-        </HtmlClassNameProvider>
-      </DocProvider>
+              </div>
+            ) : null}
+          </div>
+        </DocItemLayout>
+      </HtmlClassNameProvider>
+    </DocProvider>
   );
 }
