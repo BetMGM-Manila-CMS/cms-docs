c77340d0893533a88ab7f6d619daf13fe077f66f
Kyle-Atienza <kylefrancisedison@gmail.com>
2024-06-20 09:50:23 +0800

    fix sidebar

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'da9696e46e2fa32edbb7287a0b9bbd8bf99ecc0f', queries,  });
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/7eb9602a-63c1-49ab-986c-560697edb96a/github/main', token: 'da9696e46e2fa32edbb7287a0b9bbd8bf99ecc0f', queries,  });
 export default client;
   
