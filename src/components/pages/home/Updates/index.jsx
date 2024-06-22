f8dfac126136e6223369b9ac691bda81d3ea9be0
Kyle-Atienza <atienzakylefrancis@gmail.com>
2024-06-22 17:45:19 +0800

    add content team page

--- src/components/pages/home/Updates/index.jsx
+++ src/components/pages/home/Updates/index.jsx
@@ -1,20 +1,19 @@
-import { UpdateCard } from "./UpdateCard"
+import { UpdateCard } from "./UpdateCard";
 
 export default function Updates({ updates }) {
-
-    return (
-        <section className="container">
-            <div className="pb-8 px-4 mx-auto max-w-screen-xl sm:pb-16 lg:px-6">
-                <h2 className="text-center md:text-start">Latest Updates</h2>
-                <div className="space-y-8 md:grid lg:grid-cols-2 md:gap-6 md:space-y-0">
-                    {
-                        updates && (
-                            updates.slice(0, 2).map((update, index) => <UpdateCard key={index} update={update} />)
-                        )
-                    }
-
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className="container">
+      <div className="max-w-screen-xl px-4 pb-8 mx-auto sm:pb-16 lg:px-6">
+        <h2 className="text-center md:text-start">Latest Updates</h2>
+        <div className="space-y-8 md:grid lg:grid-cols-2 md:gap-6 md:space-y-0">
+          {updates &&
+            updates
+              .slice(0, 2)
+              .map((update, index) => (
+                <UpdateCard key={index} update={update} />
+              ))}
+        </div>
+      </div>
+    </section>
+  );
 }
