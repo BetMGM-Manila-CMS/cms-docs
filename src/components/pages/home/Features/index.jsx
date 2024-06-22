ee2dfd131bf09c67c50c28e524d9349bb1fa7671
Kyle-Atienza <atienzakylefrancis@gmail.com>
2024-06-22 13:56:26 +0800

    update home content

---
+++ src/components/pages/home/Features/index.jsx
@@ -0,0 +1,49 @@
+import Feature from "./Feature";
+import Link from "@docusaurus/Link";
+
+const FeatureList = [
+  {
+    label: "Get Building Your Builds",
+    description: "Know how to create the different needs for your tickets.",
+    url: "docs/builds/assets",
+    image:
+      "https://res.cloudinary.com/dlfu36fiw/image/upload/v1719035564/stock/glenn-carstens-peters-npxXWgQ33ZQ-unsplash_zg8qvw.jpg",
+  },
+  {
+    label: "Get to Know the Team",
+    description:
+      "Stay informed with our team structure and familiarize yourself with everyone on board.",
+    url: "/",
+    image:
+      "https://res.cloudinary.com/dlfu36fiw/image/upload/v1719035625/team/team_s3tuzf.jpg",
+  },
+  {
+    label: "Check Latest Updates",
+    description: "Easily stay updated with our announcement page.",
+    url: "/updates",
+    image:
+      "https://res.cloudinary.com/dlfu36fiw/image/upload/v1719035557/stock/oleg-laptev-QRKJwE6yfJo-unsplash_jpq9sm.jpg",
+  },
+];
+
+export default function () {
+  return (
+    <section className="container">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto sm:py-16 lg:px-6">
+        <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-6 md:space-y-0">
+          {FeatureList.map((feature, index) => {
+            return (
+              <Feature
+                label={feature.label}
+                description={feature.description}
+                url={feature.url}
+                image={feature.image}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Layout from "@theme/Layout";
 import Updates from "@site/src/components/pages/home/Updates";
 import Brands from "@site/src/components/pages/home/Brands";
 import Tools from "@site/src/components/pages/home/Tools";
+import Features from "@site/src/components/pages/home/Features";
 
 // import Hero from "@site/src/components/Hero";
 import { Hero, HomeFeatures } from "@site/src/components";
@@ -15,8 +16,22 @@ export default function Home({ recentPosts }) {
       title="CMS Knowledge Hub"
       description="The Official Documentation for BetMGM Creatives Manila CMS Team"
     >
-      <Hero />
-      <HomeFeatures />
+      <Hero className="pb-24 -mb-44">
+        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-12">
+          <div className="inline-flex items-center justify-between px-4 py-1 text-sm text-gray-700 bg-gray-100 rounded-full mb-7 dark:bg-gray-800 dark:text-white">
+            BetMGM Creatives Manila
+          </div>
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-primary">
+            CMS Team Hub
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-white">
+            This hub will be your go-to resource for navigating CMS development
+            at BetMGM Creatives Manila. Packed with insights, best practices,
+            and troubleshooting tips.
+          </p>
+        </div>
+      </Hero>
+      <Features />
       <Tools />
       <Brands />
       <Updates updates={recentPosts} />
