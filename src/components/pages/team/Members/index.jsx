9704775d06ff8a1b0dc031d7f360e7379ca279b3
Kyle-Atienza <kylefrancisedison@gmail.com>
2024-07-30 17:00:45 +0800

    re arrange members

--- src/components/pages/team/Members/index.jsx
+++ src/components/pages/team/Members/index.jsx
@@ -2,6 +2,20 @@ import teamData from "@site/config/team";
 
 export default function Member() {
   const members = teamData.members
+  const [manilaMembers, usMembers] = members.reduce((sortedMembers, member) => {
+    if (member.isProbation) {
+      return sortedMembers
+    }
+
+    if (member.officeLocation === "Manila") {
+      sortedMembers[0].push(member)
+    } else {
+      sortedMembers[1].push(member)
+    }
+    return sortedMembers
+  }, [[], []])
+
+  console.log(manilaMembers, usMembers)
 
   return (
     <section className="container relative mb-8 lg:mb-16">
@@ -10,8 +24,31 @@ export default function Member() {
           Members
         </h2>
         <div className="grid grid-cols-2 gap-6 mt-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
-          {members
-            .filter((member) => !member.isProbation)
+          {manilaMembers
+            .map(({ name, title, image }, index) => {
+              return (
+                <div className="group">
+                  <div className="mb-2 overflow-hidden bg-white rounded-lg aspect-square">
+                    {image && (
+                      <img
+                        src={image}
+                        alt=""
+                        className="object-cover w-full h-full duration-500 group-hover:scale-110"
+                      />
+                    )}
+                  </div>
+                  <p className="mb-2 font-extrabold leading-none tracking-tight text-white text-gray-900 text-md md:text-lg lg:text-xl">
+                    {name}
+                  </p>
+                  <p className="text-sm leading-none tracking-tight text-white text-gray-900 md:text-lg">
+                    {title}
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+        <div className="grid grid-cols-2 gap-6 mt-8 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+          {usMembers
             .map(({ name, title, image }, index) => {
               return (
                 <div className="group">
