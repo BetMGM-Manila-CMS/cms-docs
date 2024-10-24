1c47632938ace58ca156c1a6cd1a591c64d2c5a2
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-10-24 19:03:07 +0800

    Purge unused images from Docusaurus template
    These images are provided when scaffolding a new Docusaurus website. I
    also deleted the HomepageFeatures component which references some of the
    images, but is not used anywhere in the code.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,8 +82,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
       colorMode: {
         defaultMode: "dark",
         disableSwitch: false,
